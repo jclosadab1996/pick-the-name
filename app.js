c6552e70561241cacc3ea797ec2a7ebbfb3bc0b8
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2024-11-09 00:55:28 -0500

    Cambios

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 // app.js
-const dotenv = require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const clientRoutes = require('./routes/clientRoutes');
-const registroRoutes = requiere('./routes/registroruta');
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const clientRoutes = require("./routes/clientRoutes");
+const registroRoutes = requiere("./routes/registroruta");
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Conectar a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Conectado a MongoDB Atlas'))
-  .catch(error => console.error('Error de conexión:', error));
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Conectado a MongoDB Atlas"))
+  .catch((error) => console.error("Error de conexión:", error));
 
 // Rutas
-app.use('/api/clients', clientRoutes);
-app.use('/api/registroruta',registroRoutes);
+app.use("/api/clients", clientRoutes);
+app.use("/api/registroruta", registroRoutes);
 
 // Servir HTML principal
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/desktop/bootcamp/pick-the-name/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
